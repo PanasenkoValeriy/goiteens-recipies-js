94fd499155106b4c8e055820da097b44b12e7485
PanasenkoValeriy <124078834+PanasenkoValeriy@users.noreply.github.com>
2025-03-13 03:24:35 +0100

    upup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import "./App.css";
-import { Recipe } from "./components/Recipe/Recipe";
-import recipies from "./recipies.json";
+import React from "react";
 
 function App() {
-  return <Recipe recipe={recipies} />;
+  const myName = "Валерій";
+  const favWebsite = { name: "Google", url: "https://www.google.com/" };
+
+  const firstNumber = 349;
+  const secondNumber = 247;
+  const sum = firstNumber + secondNumber;
+
+  const colors = ["Червоний", "Синій", "Зелений"];
+
+  return (
+    <div className="App">
+      <h1>Моє ім'я: {myName}</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+
+      <img
+        src="https://cdn.pixabay.com/photo/2013/01/29/00/47/google-76517_1280.png"
+        alt="sky"
+        width="500"
+      />
+
+      <h4>
+        улюблений сайт:{" "}
+        <a href={favWebsite.url} target="_blank">
+          {favWebsite.name}
+        </a>
+      </h4>
+
+      <h4>Сума чисел:</h4>
+      <p>
+        {firstNumber} + {secondNumber} = {sum}
+      </p>
+
+      <h4>Список кольорів:</h4>
+
+      <ul>
+        {colors.map((color, index) => (
+          <li key={index}>{color}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
